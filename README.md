db24c928d7086d8016ebed8a3864d9c753f175c0
SolankiD1 <DhavalSolanki75@gmail.com>
2020-05-07 15:34:41 -0500

    updated blog app and README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MERN
+
 # DEMO of Mongo Express React NodeJS for microservices
+	- when you clone the project make sure you rerun npm install in the 'post' and comments directory'
+	- once you do so in the post and comments directory run npm start to start the services.
+	- move to the client directory and run npm start to start the React app. 
+
--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,13 +1,14 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
+  const [posts, setPosts] = useState({});
 
-  const[posts,setPosts] = useState({});
-
-  const fetchPosts = async () =>{
+  const fetchPosts = async () => {
     const res = await axios.get('http://localhost:4000/posts');
+
     setPosts(res.data);
   };
 
@@ -15,25 +16,25 @@ export default () => {
     fetchPosts();
   }, []);
 
-  const renderedPost = Object.values(posts).map(post => {
-    return(
-    <div className="card"
-         style = {{width: '30%', marginBottom: '20px'}}
-         key={post.id} >
+  const renderedPosts = Object.values(posts).map(post => {
+    return (
+      <div
+        className="card"
+        style={{ width: '30%', marginBottom: '20px' }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
+        </div>
+      </div>
+    );
+  });
 
-    <div className="card-body">
-      <h3>{post.title}</h3>
-      <CommentCreate postId={post.id}/>
-    </div>
-
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
     </div>
   );
-});
-
-  return(
-  <div className="d-flex flex-row flex-wrap justify-content-between">
-    {renderedPost}
-  </div>
-);
-
 };
