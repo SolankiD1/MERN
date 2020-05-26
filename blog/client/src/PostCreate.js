8381493861c16dbd2582fb88a6d9ac3ae400a2dc
SolankiD1 <DhavalSolanki75@gmail.com>
2020-05-26 12:54:08 -0500

    updated app

--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-
 export default () => {
-
   const [title, setTitle] = useState('');
 
-  const onSubmit = async (event) => {
-
+  const onSubmit = async event => {
     event.preventDefault();
 
     await axios.post('http://localhost:4000/posts', {
@@ -17,18 +14,19 @@ export default () => {
     setTitle('');
   };
 
-  return(
-  <div>
-   <form onSubmit={onSubmit}>
-
-    <div className="form-group">
-        <label> Title </label>
-        <input value={title} onChange={e => setTitle(e.target.value)} className="form-control" />
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
-
-    <button className="btn btn-primary">Submit</button>
-
-   </form>
-  </div>
-
-)};
+  );
+};
--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,19 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-const commentsByPostId={};
+ 
+const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
-  const commentId = randomBytes(2).toString('hex');
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
@@ -23,10 +24,24 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  res.status(201).send(comments);
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  });
 
+  res.status(201).send(comments);
 });
 
-app.listen(4001, ()=>{
-  console.log("listening on 4001");
+app.post('/events', (req, res) => {
+  console.log('Event Received:', req.body.type);
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
 });
